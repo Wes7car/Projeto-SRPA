067b7519c076613fecff2c97522e9b9a007cf1c8
Wes7car <wesleycarvalho15155@gmail.com>
2025-01-16 09:39:45 -0300

    atualizando cap 3

--- js/script.js
+++ js/script.js
@@ -27,33 +27,6 @@ document.querySelectorAll('.gallery-item img').forEach(image => {
     });
 });
 
-// Função para criar os festões
-function explodeConfetti() {
-    const numberOfConfetti = 100;  // Define quantos festões aparecerão
-    const colors = ['#FF6347', '#FFD700', '#98FB98', '#87CEEB', '#DDA0DD']; // Cores dos festões
-
-    for (let i = 0; i < numberOfConfetti; i++) {
-        const confetti = document.createElement('div');
-        confetti.classList.add('confetti');
-
-        // Definir posição inicial aleatória
-        confetti.style.left = `${Math.random() * window.innerWidth}px`;
-        confetti.style.top = `${Math.random() * window.innerHeight}px`;
-        confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-
-        // Animação de explosão
-        document.body.appendChild(confetti);
-
-        // Remover o festão após a animação
-        setTimeout(() => {
-            confetti.remove();
-        }, 2500);
-    }
-}
-
-// Chama a função para explodir festões assim que o site carregar
-window.onload = explodeConfetti;
-
 // Função para tocar o vídeo quando o botão for clicado
 document.getElementById("playButton").addEventListener("click", function () {
     var video = document.getElementById("musica-video");
